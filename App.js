d389ab66e1e8dec21dcea12b6011c511da1862c4
Norbius96 <mrnorbius3@gmail.com>
2020-06-04 11:43:03 +0200

    End Nav and nav styles. JS added

---
+++ App.js
@@ -0,0 +1,26 @@
+window.onscroll = () => {
+    const nav = document.querySelector('.nav');
+    if (this.scrollY <= 20) nav.style.background = 'transparent'; else nav.style.background = '#fff';
+};
+
+let mainNavLinks = document.querySelectorAll(".nav-item > a");
+let mainSections = document.querySelectorAll("section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY + 400;
+    mainNavLinks.forEach(link => {
+        const section = document.querySelector(link.hash);
+
+        if (
+            section.offsetTop < fromTop &&
+            section.offsetTop + section.offsetHeight > fromTop
+        ) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
